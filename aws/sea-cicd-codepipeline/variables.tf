77d2c4d71fb75c92b1ee53468e874d910cf5ae9a
Rocio Philipps <yonepv@hotmail.com>
2023-05-15 14:23:03 -0400

    sea-cicd-codepipeline
    Signed-off-by: Rocio Philipps <yonepv@hotmail.com>

---
+++ aws/sea-cicd-codepipeline/variables.tf
@@ -0,0 +1,19 @@
+variable "aws_codecommit_repository_default_branch" {
+    type = string
+    description = "Branch du repo AWS CodeCommit de la définition de l'image docker de l'application."
+}
+
+variable "aws_codecommit_repository_name" {
+    type = string
+    description = "Nom du repo AWS CodeCommit de la définition de l'image docker de l'application."  
+}
+
+variable "ecs_cluster_name" {
+    type = string
+    description = "Nom du cluster ECS"  
+}
+
+variable "ecs_service_name" {
+    type = string
+    description = "Nom du service ECS"  
+}
