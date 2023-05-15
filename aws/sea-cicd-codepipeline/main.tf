b00dd34726605c977215c6678cfac4aa53cc8dff
Rocio Philipps <yonepv@hotmail.com>
2023-05-15 17:12:33 -0400

    sea-cicd-codepipeline modification du nom de bucket
    Signed-off-by: Rocio Philipps <yonepv@hotmail.com>

--- aws/sea-cicd-codepipeline/main.tf
+++ aws/sea-cicd-codepipeline/main.tf
@@ -35,7 +35,7 @@ resource "aws_iam_role_policy_attachment" "pipeline_role_policy_attach" {
 }
 
 resource "aws_s3_bucket" "codepipeline_bucket" {
-  bucket = "${local.name}-codepipeline-bucket"
+  bucket = "${local.name}2-codepipeline-bucket" #to be modified
 }
 
 resource "aws_codepipeline" "codepipeline" {
