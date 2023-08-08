b367bc94c9795d7f12df643702900f31c612a363
Rocio Philipps <yonepv@hotmail.com>
2023-08-08 06:59:16 -0400

    adjust codebuild secret name
    Signed-off-by: Rocio Philipps <yonepv@hotmail.com>

--- aws/sea-cicd-codebuild/main.tf
+++ aws/sea-cicd-codebuild/main.tf
@@ -15,7 +15,7 @@ resource "aws_codecommit_repository" "img_definition_repository" {
 #-------------------------------------------------------------------------------
 # Secrets
 resource "aws_secretsmanager_secret" "codebuild_secret" {
-  name = "${local.name}-codebuild-secret1"
+  name = "${local.name}-codebuild-secret"
 }
 
 resource "aws_secretsmanager_secret_version" "codebuild_secret" {
