f6827300bc0402c6631e28209bde6d165c363da7
Francis Gagné <francis.gagne@mcn.gouv.qc.ca>
2024-04-24 08:50:16 -0400

    exemple aws-sea: Ajustement avec dernières modifications
    Signed-off-by: Francis Gagné <francis.gagne@mcn.gouv.qc.ca>

--- examples/aws-sea/main.tf
+++ examples/aws-sea/main.tf
@@ -5,10 +5,12 @@ locals {
 
 module "ceai_lib" {
   source = "github.com/CQEN-QDCE/ceai-cqen-terraform-lib?ref=dev"
+  #source = "../.."
 }
 
 module "sea_network" {
   source = "./.terraform/modules/ceai_lib/aws/sea-network"
+  #source = "../../aws/sea-network"
   
   aws_profile = var.aws_profile
   workload_account_type = var.workload_account_type
@@ -16,19 +18,19 @@ module "sea_network" {
 
 module "mysql" {
   source = "./.terraform/modules/ceai_lib/aws/sea-rds-aurora-mysql"
+  #source = "../../aws/sea-rds-aurora-mysql"
   
   sea_network           = module.sea_network
   identifier            = local.name
   db_name               = var.system
   db_user               = var.system
-  allocated_storage     = 20
-  max_allocated_storage = 100
   min_capacity          = 0.5
   max_capacity          = 2
 }
 
 module "ecs_cluster" {
   source = "./.terraform/modules/ceai_lib/aws/sea-ecs-cluster"
+  #source = "../../aws/sea-ecs-cluster"
   
   identifier = local.name
 }
@@ -37,7 +39,7 @@ data "template_file" "container_test" {
   template = file("${path.module}/tasks/task.json.tftpl")
   vars = {
     container_name       = local.container_name
-    image                = "rhel-minimal:latest"
+    image                = "quay.io/fedora/fedora-minimal:latest"
     awslogs_group        = "/ecs/test-task"
     container_port       = 8080
     healthckeck_path     = "/healthcheck"
@@ -51,6 +53,7 @@ data "template_file" "container_test" {
 
 module "ecs_service" {
   source = "./.terraform/modules/ceai_lib/aws/sea-ecs-fargate-service"
+  #source = "../../aws/sea-ecs-fargate-service"
   
   sea_network = module.sea_network
   identifier  = local.name
@@ -73,4 +76,23 @@ module "ecs_service" {
       mount_path = "/opt/exemple/"
     }
   }
+}
+
+module "sea_backup" {
+  source = "./.terraform/modules/ceai_lib/aws/sea-backup"
+  #source = "../../aws/sea-backup"
+
+  identifier             = local.name
+  backup_rules         = [
+    {
+        name                                  = "snapshot_mensuel"
+        schedule                              = "cron(40 16 ? * 1 *)"
+        start_window                          = 60
+        completion_window                     = 180
+        delete_after                          = 30
+        enable_continuous_backup              = false
+    }
+  ]
+  backup_alarms_email    = "test@example.com"
+  ressources_arn         = setunion([ module.mysql.cluster_rds_arn ], module.ecs_service.ecs_task_volume_arn_list)
 }
