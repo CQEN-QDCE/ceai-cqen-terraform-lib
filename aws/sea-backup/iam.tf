957118bf6fc9c7d110b2db4a4bfc766313dcd523
Rocio Philipps <rocio.philippsvalencia@mcn.gouv.qc.ca>
2025-06-23 17:00:48 -0400

    wip fix checkovs for rds and backup
    Signed-off-by: Rocio Philipps <rocio.philippsvalencia@mcn.gouv.qc.ca>

--- aws/sea-backup/iam.tf
+++ aws/sea-backup/iam.tf
@@ -66,7 +66,7 @@ data "aws_iam_policy_document" "sns_topic_policy" {
 
     principals {
       type        = "AWS"
-      identifiers = ["backup.amazonaws.com"]
+      identifiers = ["*"]
     }
 
     resources = [
