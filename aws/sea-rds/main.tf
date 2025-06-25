46c6fd78aad3091528ebac565f89db89179e8da7
Rocio Philipps <rocio.philippsvalencia@mcn.gouv.qc.ca>
2025-06-25 11:55:39 -0400

    wip fix checkovs for rds and backup - revert added kms key to secret (suggestion checkov) - no access denied exception
    Signed-off-by: Rocio Philipps <rocio.philippsvalencia@mcn.gouv.qc.ca>

--- aws/sea-rds/main.tf
+++ aws/sea-rds/main.tf
@@ -18,7 +18,6 @@ resource "random_password" "db_password" {
 
 resource "aws_secretsmanager_secret" "rds_secret" {
   name       = "${local.name}-rds-secret-${local.timestamp}"
-  kms_key_id = data.aws_kms_key.rds.arn
 }
 
 resource "aws_secretsmanager_secret_version" "rds_secret" {
