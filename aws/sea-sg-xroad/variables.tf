3fe58fe390b4bf9f0b0ad06cffea2b99b4966b48
pierre.jiji <pierre.jiji@mcn.gouv.qc.ca>
2025-11-15 20:00:53 -0500

    SG de xroad
    Signed-off-by: pierre.jiji <pierre.jiji@mcn.gouv.qc.ca>

--- aws/sea-sg-xroad/variables.tf
+++ aws/sea-sg-xroad/variables.tf
@@ -4,17 +4,17 @@ variable "aws_region" {
   default     = "ca-central-1"
 }
 
-variable "aws_profile" {
-  description = "Porifle AWS à utiliser pour la création des ressources"
-  type        = string
-  default     = null
-}
+# variable "aws_profile" {
+#   description = "Porifle AWS à utiliser pour la création des ressources"
+#   type        = string
+#   default     = null
+# }
 
-variable "aws_assume_role_workload" {
-  description = "Rôle à assumer pour le compte de travail"
-  type        = string
-  default     = null
-}
+# variable "aws_assume_role_workload" {
+#   description = "Rôle à assumer pour le compte de travail"
+#   type        = string
+#   default     = null
+# }
 
 variable "workload_account_type" {
   type        = string
