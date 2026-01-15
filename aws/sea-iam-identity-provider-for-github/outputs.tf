51051ae1cbf9feab137c11d1f50baaa45356af54
pierre.jiji <pierre.jiji@mcn.gouv.qc.ca>
2026-01-15 11:39:43 -0500

    Mettre le output temp en commentaire
    Signed-off-by: pierre.jiji <pierre.jiji@mcn.gouv.qc.ca>

--- aws/sea-iam-identity-provider-for-github/outputs.tf
+++ aws/sea-iam-identity-provider-for-github/outputs.tf
@@ -3,7 +3,7 @@ output "iam_github_action_idp_arn" {
   description = "ARN du fournisseur d'identité IAM OIDC pour Github Actions"
 }
 
-output "iam_github_action_role_arn" {
-  value       = var.create_role ? aws_iam_role.iam_role[0].arn : null
-  description = "ARN du role IAM associé au fournisseur d'identité pour GitHub Actions"
-}
+# output "iam_github_action_role_arn" {
+#   value       = var.create_role ? aws_iam_role.iam_role[0].arn : null
+#   description = "ARN du role IAM associé au fournisseur d'identité pour GitHub Actions"
+# }
