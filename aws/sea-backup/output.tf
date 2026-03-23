acbb64040f42fa3bbf026c5423735a9b18cb6717
pierre.jiji <pierre.jiji@mcn.gouv.qc.ca>
2026-03-23 16:21:44 -0400

    Ajouter output iam_role_backup_arn
    Signed-off-by: pierre.jiji <pierre.jiji@mcn.gouv.qc.ca>

--- aws/sea-backup/output.tf
+++ aws/sea-backup/output.tf
@@ -7,3 +7,8 @@ output "vault_arn" {
   value       = aws_backup_vault.backup_vault.arn
   description = "ARN de la vault de sauvegarde AWS Backup."
 }
+
+output "iam_role_backup_arn" {
+  value       = aws_iam_role.aws_backup_service_role.arn
+  description = "ARN du rôle IAM pour le service AWS Backup."
+}
